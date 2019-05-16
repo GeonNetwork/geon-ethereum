db19a13d054db0113b3483e764e282e6a9c3e3bf
busypeter <busypeter@live.com>
2019-05-16 12:24:48 +0200

    Upgrade OpenZeppelin, solc, and truffle

--- contracts/Geon/GeonStorage.sol
+++ contracts/Geon/GeonStorage.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.2;
+pragma solidity 0.5.8;
 
 import "openzeppelin-solidity/contracts/access/roles/WhitelistedRole.sol";
 import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
